f890ec00d44756c7c32debe3e4cec5790c4e178b
Miguel Medina <mmedinam1600@gmail.com>
2025-06-29 22:40:21 -0600

    feat: add cors

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from core.config import settings
 from api import router
 from core.lifespan import lifespan
@@ -8,6 +9,16 @@ def create_application() -> FastAPI:
     application = FastAPI(
         title=settings.PROJECT_NAME, version=settings.VERSION, lifespan=lifespan
     )
+
+    # Configuración de CORS para que se pueda hacer peticiones desde el frontend de cualquier origen
+    application.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
     application.include_router(router)
     return application
 
